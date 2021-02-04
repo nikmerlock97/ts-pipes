4bc716872d9691dca0dfc03d0ba1ab77b89ef2e8
nikmerlock97 <nikmerlock97@gmail.com>
2021-02-03 20:13:25 -0600

    fixed spread op

--- src/currency/commas.js
+++ src/currency/commas.js
@@ -1,9 +1,10 @@
 "use strict";
 // import { isString } from './../helpers';
 Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
 function commas(num, currency, cents) {
     var numString = num.toString() || '';
-    var numCharacters = [numString];
+    var numCharacters = tslib_1.__spread(numString);
     var withCommas = numCharacters.reduce(function (prev, cur, i) {
         if (i === 0) {
             return prev + cur;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,24 @@
 "use strict";
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spread = (this && this.__spread) || function () {
+    for (var ar = [], i = 0; i < arguments.length; i++) ar = ar.concat(__read(arguments[i]));
+    return ar;
+};
 var assert = require('assert');
 var howLongTillLunch = require('..');
 var MockDate = /** @class */ (function () {
@@ -32,7 +52,7 @@ function test(hours, minutes, seconds, expected) {
     now.setHours(hours);
     now.setMinutes(minutes);
     now.setSeconds(seconds);
-    assert.equal(howLongTillLunch.apply(void 0, lunchtime), expected);
+    assert.equal(howLongTillLunch.apply(void 0, __spread(lunchtime)), expected);
     console.log("\u001B[32m\u2713\u001B[39m " + expected);
 }
 var lunchtime = [12, 30];
